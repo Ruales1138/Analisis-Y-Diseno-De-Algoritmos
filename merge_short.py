e49aab8313dc81967e3d4af748e7ade853bb2844
David Ruales <hruales1138@gmail.com>
2025-08-10 11:15:48 -0500

    commit

--- merge_short.py
+++ merge_short.py
@@ -1,6 +1,28 @@
+def merge(izquierda: list[int], derecha: list[int]) -> list[int]:
+    resultado = []
+    i = j = 0
+    while i < len(izquierda) and j < len(derecha):
+        if izquierda[i] < derecha[j]:
+            resultado.append(izquierda[i])
+            i += 1
+        else:
+            resultado.append(derecha[j])
+            j += 1
+    if i == len(izquierda):
+        resultado.extend(derecha[j:])
+    if j == len(derecha):
+        resultado.extend(izquierda[i:])
+    return resultado
+
 def merge_sort(lista: list[int]) -> list[int]:
-    if len(lista) == 0:
+    if len(lista) <= 1:
         return lista
+    mitad = len(lista) // 2
+    izquierda = lista[:mitad]
+    derecha = lista[mitad:]
+    izquierda = merge_sort(izquierda)
+    derecha = merge_sort(derecha)
+    return merge(izquierda, derecha)
 
 
 print(merge_sort([38, 27, 43, 3, 9, 82, 10]))   # → [3, 9, 10, 27, 38, 43, 82]
