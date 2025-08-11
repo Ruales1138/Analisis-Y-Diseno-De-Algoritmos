5658a715c6e955d8025674838bc3cc3823c1383c
David Ruales <hruales1138@gmail.com>
2025-08-11 10:20:09 -0500

    commit

--- palindromo_tail.py
+++ palindromo_tail.py
@@ -1,5 +1,14 @@
 def verificar_palindromo(palabra: str, i: int = None, j: int = None) -> bool:
-    pass
+    if i is None and j is None:
+        i = 0
+        j = len(palabra) - 1
+    if i == j:
+        return True
+    if i > j:
+        return True
+    if palabra[i] != palabra[j]:
+        return False
+    return verificar_palindromo(palabra, i + 1, j - 1)
     
 
 print(verificar_palindromo('ada'))          # True
