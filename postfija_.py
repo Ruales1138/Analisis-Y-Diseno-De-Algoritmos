c57e13c1832b5da4588c12dc896feabdfbbbb0ae
David Ruales <hruales1138@gmail.com>
2025-08-13 10:08:51 -0500

    commit

---
+++ postfija_.py
@@ -0,0 +1,4 @@
+def calcular_postfija(lista):
+    pass
+
+print(calcular_postfija(["3", "4", "+", "2", "*"]))
--- reverse_non_tail.py
+++ reverse_non_tail.py
@@ -1,7 +1,8 @@
-def invertir_lista(lista: list, lista_inv: list = [], i: int = 0, largo = None) -> list:
-    pass
+def invertir_lista(lista: list) -> list:
+    if len(lista) <= 1:
+        return lista
+    return [lista[-1]] + invertir_lista(lista[:-1])
 
 
-print(invertir_lista([0,1,2,3,4,5], []))
-print(invertir_lista([0,2,5,2,7,4], []))
-print(invertir_lista(['A', 'B', 'C', 'D'], []))
+print(invertir_lista([0,1,2,3,4,5]))
+print(invertir_lista(['A', 'B', 'C', 'D']))
