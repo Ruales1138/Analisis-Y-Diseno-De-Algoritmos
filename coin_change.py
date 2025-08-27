78477d76bef302df66319b1bde5e3accdf1e2443
David <hruales1138@gmail.com>
2025-08-27 11:37:57 -0500

    commit

---
+++ coin_change.py
@@ -0,0 +1,5 @@
+def coin_change(coins: list[int], amount: int) -> int:
+    return -1
+
+print(coin_change([1,2,5], 11)) # ---> 3
+print(coin_change([2], 3)) # ---> -1
--- ways.py
+++ ways.py
@@ -1,5 +1,20 @@
-def ways(lista: list[list[int]]):
-    return 
+def ways(lista: list[list[int]], memo: dict = None, i: int = 0, j: int = 0):
+    if memo is None:
+        memo = {}
+        n = len(lista)-1
+        m = len(lista[0])-1
+        memo[(n, m)] = 1
+    if (i,j) in memo:
+        return memo[(i,j)]
+    if i == len(lista):
+        return 0
+    if j == len(lista[0]):
+        return 0
+    if lista[i][j] == 1:
+        return 0
+    memo[(i,j)] = ways(lista, memo, i+1, j) + ways(lista, memo, i, j+1)
+    return memo[(i,j)]
+
 
 grid = [[0,0,0],
         [0,1,0],
