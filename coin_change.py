1695f155cc0964a1a8ed5792ad9f0cdc6a73a65e
David <hruales1138@gmail.com>
2025-08-29 09:49:01 -0500

    commit

--- coin_change.py
+++ coin_change.py
@@ -1,5 +1,13 @@
 def coin_change(coins: list[int], amount: int) -> int:
-    return -1
+    memo: dict = {}
+    def dp(coins, amount, current: int = 0, i: int = 0):
+        if coins[i] + current < amount:
+            current += coins[i]
+            print(current)
+            return dp(coins, amount, current, i)
+    return dp(coins, amount)
 
+print(coin_change([1,2,3], 3)) # ---> 3
+print()
 print(coin_change([1,2,5], 11)) # ---> 3
 print(coin_change([2], 3)) # ---> -1
