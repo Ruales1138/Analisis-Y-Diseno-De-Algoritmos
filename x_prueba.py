eadab72e17e9e216b8c2e58a544963d6b62e7d78
EstivenAlvarez <Estivenalvarez1902@hotmail.com>
2025-09-05 14:57:40 -0500

    commit

--- x_prueba.py
+++ x_prueba.py
@@ -1,39 +1,24 @@
-def longestSubstring(s: str, k: int) -> int:
-    # Caso base: si la cadena es muy corta no puede haber solución
-    if len(s) < k:
-        return 0
+def contar(lista, inicio = None, fin = None):
+    if inicio is None:
+        inicio = 0
+    if fin is None:
+        fin = len(lista)-1
+    if inicio > fin:
+        return
+    if inicio == fin:
+        num = lista[inicio]
+        if num % 2 == 0:
+            return (num, 1, 0)
+        else:
+            return (num, 0, 1)
+    mitad = (inicio + fin) // 2
+    izquierda = contar(lista, inicio, mitad)
+    derecha = contar(lista, mitad+1, fin)
+    suma = izquierda[0] + derecha[0]
+    pares = izquierda[1] + derecha[1]
+    impares = izquierda[2] + derecha[2]
 
-    # Revisar cuántas veces aparece cada letra
-    conteo = {}
-    for letra in s:
-        if letra not in conteo:
-            conteo[letra] = 0
-        conteo[letra] += 1
+    return (suma, pares, impares)
+    
 
-    # Si todos los caracteres aparecen al menos k veces, la cadena completa sirve
-    cumple = True
-    for letra in conteo:
-        if conteo[letra] < k:
-            cumple = False
-            break
-    if cumple:
-        return len(s)
-
-    # Si hay algún caracter que no cumple, lo usamos como separador
-    for letra in conteo:
-        if conteo[letra] < k:
-            # Cortamos la cadena en pedazos donde aparezca esa letra
-            partes = s.split(letra)
-            # Revisamos cada parte por separado y nos quedamos con la mayor
-            mejor = 0
-            for parte in partes:
-                longitud = longestSubstring(parte, k)
-                if longitud > mejor:
-                    mejor = longitud
-            return mejor
-
-    return 0
-
-
-print(longestSubstring("aaabb", 3))   # ➝ 3 ("aaa")
-print(longestSubstring("ababbc", 2))  # ➝ 5 ("ababb")
+print(contar([1,2,3,4,5,6]))
