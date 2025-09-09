c9526f1ea7c9340a43636eb6a5141521ee6bfd0c
David Ruales <hruales1138@gmail.com>
2025-09-09 14:02:09 -0500

    commit

--- programacion_dinamica/decode_ways.py
+++ programacion_dinamica/decode_ways.py
@@ -1,12 +1,19 @@
 def decode_ways(string):
     memo = {}
-    def dp(string, i=0):
-        if i == len(string):
+    n = len(string)
+    def dp(string, n, i=0):
+        if i in memo:
+            return memo[i]
+        if i == n:
             return 1
         if string[i] == '0':
             return 0
-        return dp(string, i+1)
-    return dp(string)
+        ways = dp(string, n, i+1)
+        if i+1 < n and 10 <= int(string[i:i+2]) <= 26 :
+            ways += dp(string, n, i+2)
+        memo[i] = ways
+        return ways
+    return dp(string, n)
 
 print(decode_ways('12')) # -> 2 (“AB”, “L”)
 print(decode_ways('226')) # -> 3 (“BBF”, “BZ”, “VF”)
