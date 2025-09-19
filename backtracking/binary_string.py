c6f3ba34ae2fc8c4880f40d2c8bed545666fcb50
Sebastian <jsuarez337@soyudemedellin.edu.co>
2025-09-19 13:38:16 -0500

    binary

--- backtracking/binary_string.py
+++ backtracking/binary_string.py
@@ -1,16 +1,14 @@
-def binary_string(num, current_branch=[], branches=[]):
+def binary(num, current_branch=[], branches=[]):
     if len(current_branch) == num:
         copy = current_branch.copy()
         branches.append(copy)
         return branches
     for i in range(0,2):
-        #if current_branch == []
-        if current_branch[-1] != 0:
-            current_branch.append(i)
-            binary_string(num, current_branch)
-            current_branch.pop()
-
+        if i == 0 and current_branch != [] and current_branch[-1] == 0:
+            continue
+        current_branch.append(i)
+        binary(num, current_branch, branches)
+        current_branch.pop()
     return branches
-    
 
-print(binary_string(3))
+print(binary(3))
