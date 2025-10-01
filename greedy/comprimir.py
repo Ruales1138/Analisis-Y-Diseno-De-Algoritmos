13798e7739b83730b5e164dd3482d41860e1df7e
David Ruales <hruales1138@gmail.com>
2025-10-01 11:30:22 -0500

    commit

---
+++ greedy/comprimir.py
@@ -0,0 +1,59 @@
+import heapq
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+    def __repr__(self):
+        return str(self.val)   
+
+def comprimir(string, valores=None, nuevo_nodo=None):
+    if valores is None:
+        frecuencia = {}
+        for letra in string:
+            if letra not in frecuencia:
+                frecuencia[letra] = 1
+            else:
+                frecuencia[letra] += 1
+        print(frecuencia)
+        valores = []
+        for clave, valor in frecuencia.items():
+            valores.append((valor, clave))
+        heapq.heapify(valores)
+    print('v', valores)
+    if len(valores) > 1:
+        valor_1 = heapq.heappop(valores)
+        valor_2 = heapq.heappop(valores)
+        if valor_1[1] == '*':
+            nodo_1 = nuevo_nodo
+        else:
+            nodo_1 = TreeNode(valor_1)
+        if valor_2[1] == '*':
+            nodo_2 = nuevo_nodo
+        else:
+            nodo_2 = TreeNode(valor_2)
+        suma = valor_1[0] + valor_2[0]
+        nuevo_valor = (suma, '*')
+        nuevo_nodo = TreeNode(nuevo_valor)
+        nuevo_nodo.left = nodo_1
+        nuevo_nodo.right = nodo_2
+        print(nuevo_nodo.left, nuevo_nodo, nuevo_nodo.right)
+        heapq.heappush(valores, nuevo_valor)
+        return comprimir(string, valores, nuevo_nodo)
+    else:
+        return nuevo_nodo
+    
+def print_tree(node, prefix="", is_left=True):
+    if node is not None:
+        print_tree(node.right, prefix + ("│   " if is_left else "    "), False)
+        print(prefix + ("└── " if is_left else "┌── ") + str(node.val))
+        print_tree(node.left, prefix + ("    " if is_left else "│   "), True)
+
+arbol = comprimir('ABDDDCABCADAA')
+print()
+print_tree(arbol)
+arbol_2 = comprimir('BCAADDDCCACACAC', None, None)
+print()
+print_tree(arbol_2)
---
+++ greedy/test.py
@@ -0,0 +1,5 @@
+import heapq
+
+d = [(3,"b"),(3,"a"),(3,"b")]
+heapq.heapify(d)
+print(d)
